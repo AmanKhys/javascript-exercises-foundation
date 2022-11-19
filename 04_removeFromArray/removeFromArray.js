0fe26a2a4247c906efdb2b87016a0633e2262904
AmanKhys <amankhys2112@gmail.com>
2022-11-19 17:34:18 +0530

    non working multi argument code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function(arr,subArr) {
-  for(let i=0;i<arr.length-1;i++) if(arr[i]==subArr) arr.splice(i,1);
+const removeFromArray = function() {
+  let arr = arguments[0];
+  len = arguments.length -1;
+  for(let j=1;j<len;j++)
+    for(let i=0;i<arr.length-1;i++) 
+      if(arr[i]==arguments[j]) arr.splice(i,1);
   return arr;
 };
 
