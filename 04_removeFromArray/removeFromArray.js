4782afff423a70e861d6c50ea1e738df1a1fe8f4
AmanKhys <amankhys2112@gmail.com>
2022-11-19 17:15:17 +0530

    04-removeFromArray.js first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,subArr) {
+  for(let i=0;i<arr.length-1;i++) if(arr[i]==subArr) arr.splice(i,1);
+  return arr;
 };
 
 // Do not edit below this line
