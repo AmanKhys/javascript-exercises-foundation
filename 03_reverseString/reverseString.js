d73c5ca57157b24cc435229a0d2535f8dab52e58
AmanKhys <amankhys2112@gmail.com>
2022-11-19 16:45:23 +0530

    03-reverseString.js test-1 passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+let string = 'hello'
+const reverseString = function(string) {
+  let revString = '';
+  for(let i=string.length -1;i>=0;i--) revString +=string.substring(i,i+1);
+  return revString;
+  }
 
 // Do not edit below this line
 module.exports = reverseString;
