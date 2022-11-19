d9dcfc7496c560ab7d865417c9b16556f03bf77d
AmanKhys <amankhys2112@gmail.com>
2022-11-19 13:06:22 +0530

    02-repeatString.js test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+string = 'hey';num = 1;
+const repeatString = function(string,num ) {
+  if(num<0) return 'ERROR';
+  value = ''
+  for(;num>0;num--) value += string;
+  return value;
 };
 
 // Do not edit below this line
