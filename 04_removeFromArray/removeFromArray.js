6f3ee38e2c4df8d02d577aec99b9b8e5dab32371
AmanKhys <amankhys2112@gmail.com>
2022-11-20 13:21:26 +0530

    04-removeFromArray.js all test pass, same type only delete pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,7 +3,7 @@ const removeFromArray = function(arr,...Args) {
 
   for(let i = 0;i<Args.length ;i++){
     for(let j = 0; j<arr.length  ; j++){
-      if(arr[j]==Args[i]) arr.splice(j,1);
+      if(arr[j]===Args[i]) arr.splice(j,1);
     }
   }
   return arr;
