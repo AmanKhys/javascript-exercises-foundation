1b60218f6b7bc5979224897d2ee2b1d34284aeee
AmanKhys <amankhys2112@gmail.com>
2022-11-20 16:05:31 +0530

    05_sumAll.js 06_leapYears.js all pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+  if(number1 <0 || number2 <0 ||
+    typeof number1 !=typeof 2 ||
+    typeof number2 !=typeof 2 ) return 'ERROR';
+  let tempNumber, sum = 0;
+  if(number1> number2){
+    tempNumber = number1;number1=number2;number2=tempNumber;}
+  for(;number1<=number2;number1++) sum += number1;
+  return sum;
 };
 
 // Do not edit below this line
