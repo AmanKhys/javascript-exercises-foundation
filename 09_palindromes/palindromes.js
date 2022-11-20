fe92e0f751d7c9c53f2882541f2d4b0a616600ef
AmanKhys <amankhys2112@gmail.com>
2022-11-20 23:36:11 +0530

    09_palindromes.js all test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let len = string.length;
+  let reverseString = '';
+  for(let i = len - 1; i>=0;i--){
+    reverseString += string.substring(i,i+1);
+  }
+  return reverseString===string;
 };
 
 // Do not edit below this line
