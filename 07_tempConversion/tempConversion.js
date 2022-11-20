3b4c9f2970f6ed2b19c45613b3db6a50d72f4cbf
AmanKhys <amankhys2112@gmail.com>
2022-11-20 16:50:24 +0530

    07_tempConversion.js all test pass"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ctof = function(cel) {
+ let far = cel*(9/5) + 32;
+ let rounded = far.toFixed(1);
+ return Number(rounded);
 
 };
 
-const ctof = function() {
-
+const ftoc = function(far) {
+  let cel = (far - 32) * (5/9);
+  let rounded = cel.toFixed(1);
+  return Number(rounded);
 };
 
 // Do not edit below this line
