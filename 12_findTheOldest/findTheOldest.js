b97e010f6321843b0615ff120c83c1c8bb6f9749
AmanKhys <amankhys2112@gmail.com>
2022-11-22 08:34:18 +0530

    12_findTheOldest.js all test passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+  //storing ages of people in array with same index
+  let ageArr = [], oldest;
+  for(let i = 0; i<people.length;i++){
+    let person = people[i];
+    let birth = person.yearOfBirth;
+    let death = person.yearOfDeath;
+    if(!death) death = new Date().getFullYear();
+    ageArr[i] = death - birth;
+  } 
+  //oldest array index number finding
+  oldest = 0;
+  for(let i = 0;i<people.length - 1;i++)
+    if(ageArr[i]<ageArr[i+1]) oldest = i+1;
+  
+  
+  let oldestPerson = people[oldest];
+  return oldestPerson;
 };
 
 // Do not edit below this line
