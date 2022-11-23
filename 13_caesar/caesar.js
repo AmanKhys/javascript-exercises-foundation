12c7fe23f897acc49bf0426be95a64f7fa426518
AmanKhys <amankhys2112@gmail.com>
2022-11-23 19:56:35 +0530

    13_caesar.js 2 test passed

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,13 @@
-const caesar = function() {
-
+const caesar = function(string, shiftKey) {
+  let len = string.length;
+  let shiftString = '';
+  for(let i = 0; i<len; i++){
+      shiftString += String.fromCharCode(string.charCodeAt(i)+shiftKey);
+  }
+  return shiftString;
 };
 
 // Do not edit below this line
 module.exports = caesar;
+
+
