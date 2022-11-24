47ac640da6f0beffe3a4e04df1df58002fe7ea78
AmanKhys <amankhys2112@gmail.com>
2022-11-24 08:05:47 +0530

    13_caesar.js 3 test passed, shift with -ve numbers not working properly

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -2,12 +2,35 @@ const caesar = function(string, shiftKey) {
   let len = string.length;
   let shiftString = '';
   for(let i = 0; i<len; i++){
-      shiftString += String.fromCharCode(string.charCodeAt(i)+shiftKey);
-  }
+    let code = string.charCodeAt(i);
+    let sChar = (code, shiftKey) => String.fromCharCode(code+shiftKey);
+    let char = (i) => string.charAt(i);
+    if((code>=65 && code <=122)){
+      //for letters overflowing right side
+      if((code + shiftKey > 90 && code <=90) || (code +shiftKey >122)){
+        let diff = 0;
+        if(code <=90){
+          diff = code + shiftKey - 90; code = 64;}
+        else if(code >=97){
+          diff = code + shiftKey - 122; code = 96;}
+        shiftString += sChar(code, diff);
+      }
+      //for letters overflowing left side 
+      else if((code + shiftKey <65) || (code +shiftKey <96 && code >=96)){
+        let diff = 0;
+        if(code <=90){
+          diff = code + shiftKey - 90; code = 90;}
+        else if(code >=96){
+          diff = code + shiftKey - 122; code = 120;}
+        shiftString += sChar(code, diff);
+      }
+      //for letters in normal
+      else shiftString += sChar(code,shiftKey);
+    }
+    else shiftString += char(i);
+    }
   return shiftString;
 };
 
 // Do not edit below this line
 module.exports = caesar;
-
-
